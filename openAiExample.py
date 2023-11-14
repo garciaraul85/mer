0c96cab673e5f001cc25872a7fe5c98dee18f8fe
raulgarcia <garcia.raul@gmail.com>
2023-11-14 17:00:25 -0500

    showed how to get output as json

--- openAiExample.py
+++ openAiExample.py
@@ -2,11 +2,13 @@ import openai
 client = openai.OpenAI()
 
 reponse = client.chat.completions.create(
-    model= "gpt-3.5-turbo",
+    model= "gpt-4-1106-preview",
     messages=[
+        {"role": "system", "content": "Provide a JSON response with the following structure: {'winner': 'string', 'event': 'string', 'year': 'number', 'runner_up': 'string', 'number_of_titles': 'number', 'final_location': 'string'} for any sports event inquiries."},
         {"role": "user", "content": "Who won the cricket world cup in 2015?"}
     ],
-    seed=1 # set the same response
+    #seed=1 # set the same response
+    response_format={"type": "json_object"}
 )
 
 print(reponse.choices[0].message.content)
