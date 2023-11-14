c878eec6aa69c86b23ea412d1d20c179af28c559
raulgarcia <garcia.raul@gmail.com>
2023-11-14 16:53:00 -0500

    seed added for consistent response.

--- openAiExample.py
+++ openAiExample.py
@@ -4,8 +4,9 @@ client = openai.OpenAI()
 reponse = client.chat.completions.create(
     model= "gpt-3.5-turbo",
     messages=[
-        {"role": "user", "content": "Who won the cricket world cup in 2019?"}
-    ]
+        {"role": "user", "content": "Who won the cricket world cup in 2015?"}
+    ],
+    seed=1 # set the same response
 )
 
 print(reponse.choices[0].message.content)
