4647eebb180a875f8bb165939dedfcb1f1dbb4af
raulgarcia <garcia.raul@gmail.com>
2023-11-14 16:33:07 -0500

    add user interface

--- openAITurboExample.py
+++ openAITurboExample.py
@@ -1,9 +1,16 @@
 import openai
+import gradio as gr
+
 client = openai.OpenAI()
 
-response = client.chat.completions.create(
-    model="gpt-4-1106-preview",
-    messages=[{"role": "user", "content": "Who won the cricket world cup in 2019?"}]
-)
+def get_open_ai_response(question):
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=[{"role": "user", "content": question}]
+    )
 
-print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
+# Create user interface for our chatbot
+iface = gr.Interface(fn=get_open_ai_response, inputs="text", outputs="text")
+iface.launch()
